7c116aa73e8beeb7127c9ef5896c9358c54f3ecd
Bang Giang <finnthuman4148@gmail.com>
2023-09-15 11:32:28 +0700

    updated code with api call

--- lib/Presentation/Screens/News/detailNewsPage.dart
+++ lib/Presentation/Screens/News/detailNewsPage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html/flutter_html.dart';
+
 import 'package:thanh_nien_da_nang/Presentation/Screens/News/newsDetailData.dart';
 
-class DetailNewsPage extends StatefulWidget {
+class DetailNewsPage extends StatelessWidget {
   final int id;
   final NewsDetailData newsData;
 
@@ -13,130 +15,105 @@ class DetailNewsPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<DetailNewsPage> createState() => _DetailNewsPageState();
-}
-
-class _DetailNewsPageState extends State<DetailNewsPage> {
-  final ScrollController _scrollController = ScrollController();
-
-  @override
   Widget build(BuildContext context) {
-    final newsData = widget.newsData;
-
     return Scaffold(
-      body: CustomScrollView(
-        controller: _scrollController,
-        slivers: <Widget>[
-          SliverAppBar(
-            elevation: 0,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        flexibleSpace: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: FlexibleSpaceBar(
             centerTitle: true,
-            pinned: true,
-            //floating: false,
-            flexibleSpace: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50),
-              child: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(
-                  newsData.title,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xff1F1F1F),
-                  ),
-                ),
+            title: Text(
+              newsData.title,
+              maxLines: 1,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w700,
+                color: Color(0xff1F1F1F),
               ),
             ),
-            backgroundColor: const Color(0xffFFFFFF),
-            leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back_ios),
-              color: Colors.black,
-            ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Container(
-                        child: Text(
+        ),
+        backgroundColor: const Color(0xffFFFFFF),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+        ),
+      ),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           newsData.title,
                           style: TextStyle(
                             fontSize: 24,
                             fontWeight: FontWeight.w700,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 15),
-                      Row(
-                        children: [
-                          Image.asset('images/calendar1.png'),
-                          SizedBox(width: 5),
-                          Text(
-                            'Đăng vào ' + newsData.date,
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff8A8A8A),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Image.asset('images/calendar1.png'),
+                            SizedBox(width: 5),
+                            Text(
+                              'Đăng vào ' + newsData.date,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff8A8A8A),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Wrap(
-                          spacing: 4,
-                          runSpacing: 8,
-                          children: newsData.categories.map((category) {
-                            return Container(
-                              padding: EdgeInsets.all(8),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Color(0xffF5F5F5),
-                              ),
-                              child: Text(
-                                category,
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color(0xff6A6A6A),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Wrap(
+                            spacing: 4,
+                            runSpacing: 8,
+                            children: newsData.categories.map((category) {
+                              return Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color(0xffF5F5F5),
                                 ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      if (newsData.content.isNotEmpty)
-                        Container(
-                          height: 400, // Adjust the height as needed
-                          color: Colors
-                              .grey, // Add a background color for debugging
-                          child: WebView(
-                            initialUrl: 'about:blank',
-                            onWebViewCreated: (WebViewController controller) {
-                              controller.loadUrl(Uri.dataFromString(
-                                newsData.content,
-                                mimeType: 'text/html',
-                              ).toString());
-                            },
+                                child: Text(
+                                  category,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff6A6A6A),
+                                  ),
+                                ),
+                              );
+                            }).toList(),
                           ),
                         ),
-                    ],
-                  ),
-                );
-              },
-              childCount: 1,
+                        SizedBox(height: 20),
+                        Html(data: newsData.content),
+                      ],
+                    ),
+                  );
+                },
+                childCount: 1,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/Presentation/Screens/News/newsDetailData.dart
+++ lib/Presentation/Screens/News/newsDetailData.dart
@@ -3,13 +3,11 @@ class NewsDetailData {
   final String date;
   final List<String> categories;
   final String content;
-  // final int id;
 
   const NewsDetailData({
     required this.title,
     required this.date,
     required this.categories,
     required this.content,
-    // required this.id,
   });
 }
